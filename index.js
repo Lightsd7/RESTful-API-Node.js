2678e42ee2017b82d079835da7a5d43ba2a0cfe9
Raphael Molina <raphael.bedran@konduto.com>
2020-07-15 17:14:38 -0300

    Finalizando project

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+// let routesIndex = require('./routes/index');
+// let routesUsers = require('./routes/users');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(expressValidator());
+
+consign().include('routes').include('utils').into(app);
+
+// app.use(routesIndex);
+// app.use('/users', routesUsers);
+
+
+app.listen(3000, '127.0.0.1', () => {
+    console.log('servidor rodando!');
+});
---
+++ utils/validator.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    user: (app, req, res) => {
+        req.assert('name', 'O nome é obrigatório.').notEmpty();
+        req.assert('email', 'O email está inválido.').notEmpty().isEmail();
+
+        let errors = req.validationErrors();
+
+        if (errors) {
+            app.utils.error.send(errors, req, res);
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
